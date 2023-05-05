c0afa12daa6b0cd2e7433f8f49a304022a810f27
Ameen Ur Rehman <ameenreh2003@gmail.com>
2023-05-05 16:04:14 +0530

    streamlit day1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import streamlit as st
+
+st.title("All in one Machine Learning Algoithms ")
+st.write("""
+##  Exploring different Machine Learning Classifier 
+### Which one is the best Classifer? Choice is yours
+""")
+
+dataset_name = st.sidebar.selectbox("Select DataSets" , ("Iris" , "Breast Cancer", "Wine DataSets"))
+st.write(dataset_name)
